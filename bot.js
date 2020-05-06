20f61ea6f107158be603b1884e6d1a8949e628c5
ShizukuMizuki <joseph.gamedev@gmail.com>
2020-05-05 19:34:06 -0700

    Test Code

--- bot.js
+++ bot.js
@@ -1349,6 +1349,20 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 // #endregion
 
                 // #endregion
+
+                // #region Message Management
+
+                case 'fkasmngfnjdoihgiehsdwtoahjsti':
+                    console.log(bot.getMessages({
+                        channelID: channelID,
+                        limit: 5
+                    }, function (err, response) {
+                            if (err) console.error(err);
+                            else console.log(response);
+                    }));
+                    break;
+
+                // #endregion
             }
             
          }
@@ -1432,48 +1446,3 @@ function removeRepeated(str, inst) {
     return str;
 }
 // #endregion
-
-// #region Stream Notifs
-
-function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" })
-        .then(function () { console.log("Sign-in successful"); },
-            function (err) { console.error("Error signing in", err); });
-}
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyCClZEJZgez9JmStrA5wZeYhZPjHDZs6bs");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function () { console.log("GAPI client loaded for API"); },
-            function (err) { console.error("Error loading GAPI client for API", err); });
-}
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute() {
-    return gapi.client.youtube.liveBroadcasts.list({
-        "part": "id,contentDetails",
-        "broadcastStatus": "active",
-        "broadcastType": "all"
-    })
-        .then(function (response) {
-            // Handle the results here (response.result has the parsed body).
-            console.log("Response", response);
-        },
-            function (err) { console.error("Execute error", err); });
-}
-/*gapi.load("client:auth2", function () {
-    gapi.auth2.init({ client_id: "YOUR_CLIENT_ID" });
-});
-*/
-function streamLoop() {
-    authenticate().then(loadClient);
-    var response = execute();
-    console.log("Response from Google API: ", response);
-    setTimeout(streamLoop, 300);
-}
-
-
-// GET https://www.googleapis.com/youtube/v3/liveBroadcasts
-// https://www.youtube.com/channel/UCycfa0gUSSpRlBT7XGtAowQ
-
-// #endregion
-
