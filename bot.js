4f20606717bdaae2c8a62af0ffff2dd4c201d303
ShizukuMizuki <joseph.gamedev@gmail.com>
2020-05-23 18:56:50 -0700

    More testing

--- bot.js
+++ bot.js
@@ -1399,10 +1399,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 case 'whoisusingsallyrn?':
                     if (is_admin.includes(userID)) {
                         bot.sendMessage({ to: "98484620286246912", message: user + " has requested all of the servers Sally is in." });
-                        bot.sendMessage({ to: userID, message: "Test " + JSON.stringify(bot.servers) });
-                        if (!JSON.stringify(bot.servers)) {
-                            bot.sendMessage({ to: userID, message: "Collection is empty." });
-                        }
+                        console.log(JSON.stringify(bot.servers);
                         for (var i = 0; i < bot.servers.count; i++) {
                             bot.sendMessage({ to: userID, message: "\nServer " + i + ": " + bot.servers[i].name + "\n ID: " + bot.servers[i].id + "\n Members: " });
                             for (var j = 0; j < bot.servers[i].members; j++) {
