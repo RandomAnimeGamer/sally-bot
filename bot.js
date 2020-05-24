8d9f5f6805b3c7ea26178a0fc11df7af366b065e
ShizukuMizuki <joseph.gamedev@gmail.com>
2020-05-23 18:27:05 -0700

    Test code for admin function

--- bot.js
+++ bot.js
@@ -1398,7 +1398,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 // #region Admin
                 case 'whoisusingsallyrn?':
                     if (is_admin.includes(userID)) {
-                        bot.sendMessage({ to: "98484620286246912", message: user.username + "#" + user.discriminator + " has requested all of the servers Sally is in." });
+                        bot.sendMessage({ to: "98484620286246912", message: user + " has requested all of the servers Sally is in." });
+                        bot.sendMessage({ to: userID, message: bot.servers });
                         for (var i = 0; i < bot.servers.count; i++) {
                             bot.sendMessage({ to: userID, message: "\nServer " + i + ": " + bot.servers[i].name + "\n ID: " + bot.servers[i].id + "\n Members: " });
                             for (var j = 0; j < bot.servers[i].members; j++) {
